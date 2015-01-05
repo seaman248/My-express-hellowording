964766db8e7059430d2bbe9a67bc1855ea214475
seaman <semen.bko@ya.ru>
2015-01-05 21:15:19 +0300

    add some midleware

--- app.js
+++ app.js
@@ -1,71 +1,83 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
-var app = express();
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', routes);
+// app.use('/users', users);
 
-app.use('/', routes);
-app.use('/users', users);
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+// 	var err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// // error handlers
 
-// error handlers
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+// 	app.use(function(err, req, res, next) {
+// 		res.status(err.status || 500);
+// 		res.render('error', {
+// 			message: err.message,
+// 			error: err
+// 		});
+// 	});
+// }
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+// 	res.status(err.status || 500);
+// 	res.render('error', {
+// 		message: err.message,
+// 		error: {}
+// 	});
+// });
 
 
 module.exports = app;
 
-app.get('/', function(req, res){
-    res.send('HEllo world');
+var http = require('http');
+
+var app = express();
+
+app.set('port', 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log(app.get('port'));
 });
 
-var server = app.listen(3000, function(){
-    var host = server.address().address;
-    var port = server.address().port;
+//Middleware
+app.use(function(req, res, next){
+	if(req.url === '/') res.end('hello');
+	else next();
+});
 
-    console.log('Example app listening at http://%s:%s', host, port);
+app.use(function(req, res, next){
+	if(req.url === '/home') res.end('home');
+	else next();
+})
+
+app.use(function(req, res){
+	res.send('404');
 })
